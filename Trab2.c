59678d34625f572dc4ef24e40e39778ff45d8461
G182500 <76542283+G182500@users.noreply.github.com>
2020-12-23 22:53:07 -0300

    Update Trab2.c

--- Trab2.c
+++ Trab2.c
@@ -4,6 +4,7 @@
 #include<semaphore.h>
 #include <unistd.h>
 #define N 20
+
 	int contPostIt=0;
 	int TAM;           // numero de usuarios
 	sem_t semaforo1 ;  //declarando os semaforos
@@ -16,9 +17,7 @@ void dorme_aleatorio(){
 }
 
 void colaPostIt(){
-
 	printf("Colando um PostIt: \n"); 
-	
 }
 
 void leva_mochila_ate_B_e_volta(){
@@ -32,8 +31,7 @@ void leva_mochila_ate_B_e_volta(){
 
 static void * usuario() {  //thread usuario		
  
- 	while(1){
- 		
+  while(1){	
  	dorme_aleatorio();
 	 	
  	sem_wait(&semaforo1);
@@ -47,31 +45,30 @@ static void * usuario() {  //thread usuario
 				printf("\n");
 				sem_post(&semaforo2);
  				sem_wait(&semaforo1);
-			 }
- 				
+		}		
  	}	
  	
 } 
 
 static void * pombo () {  //thread pombo
-
-	 while(1){
-	 	
+	
+  while(1){
 		sem_wait(&semaforo2);
 		printf("Ei pombo, acorda!! \n");
 		leva_mochila_ate_B_e_volta();
  		contPostIt = 0;
  		
 		int i;
+	  
  		for (i=0; i<N; i++){	
-		 	
  			printf("Adic. msg %d\n", i);
  		}
- 			printf("\n");
- 			printf("...O pombo voltou para A...\n");		
- 			printf("\n");
- 			sem_post(&semaforo1);
- 	}	
+	  
+ 		printf("\n");
+ 		printf("...O pombo voltou para A...\n");		
+ 		printf("\n");
+ 		sem_post(&semaforo1);
+ 	 }	
  		pthread_exit(NULL);
  	  				
 } 
@@ -81,7 +78,9 @@ int main(int argc, char *argv[]) {
 		printf("Numero de usuarios: ");
 		scanf("%d",&TAM);               //coletando o numero de usuarios
 		printf("\n");
+	
 		int j;
+	
 		sem_init(&semaforo1,0,1);	// iniciando os semaforos de controle
 		sem_init(&semaforo2,0,0);
 
@@ -89,16 +88,17 @@ int main(int argc, char *argv[]) {
 		pthread_t thPombo;              //declarando as threads pombo e usuario
 		pthread_t thUsuario[TAM];
 			
-		for (j=0 ; j<TAM ; j++){		// criando o grupo de trheads usuario
-			pthread_create (&thUsuario[j] , NULL , *usuario , NULL);
+		for (j=0 ; j<TAM ; j++){		
+			pthread_create (&thUsuario[j] , NULL , *usuario , NULL);   // criando o grupo de trheads usuario
 		}
+	
 		pthread_create (&thPombo , NULL , *pombo , NULL);  //criando a thread pombo
 		
-		pthread_join(thPombo, NULL);       // iniciando a thread pombo
+		pthread_join(thPombo, NULL);   // iniciando a thread pombo
+	
 		for (j=0 ; j<TAM ; j++){
 			pthread_join(thUsuario[j], NULL);   //iniciando as threads usuario
 		}
 	    
-
 	return 0;
 }
